47a6440c248ee488ed5c27aa6dca1e19c67aad7d
@anulman <anulman@gmail.com>
2017-12-31 14:15:58 -0500

    feat(hyde-compiler): compile hyde files & write with broccoli

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import CachingWriter from 'broccoli-caching-writer';
+import Hyde from 'mr-hyde';
+
+import { join, sep } from 'path';
+import { readFile, outputFile, outputJSON } from 'fs-extra';
+
+export default class HydeCompiler extends CachingWriter {
+  constructor(inputNode, options) {
+    super([inputNode], {
+      annotation: options && options.annotation,
+      name: "broccoli-hyde-compiler",
+      persistentOutput: true,
+    });
+
+    this.hyde = new Hyde(options && options.name);
+  }
+
+  async build() {
+    const pathStartIndex = this.inputPaths[0].length;
+
+    let hyde = this.hyde;
+
+    this.outputPath = join(this.outputPath, 'hyde');
+
+    for (let fullpath of this.listFiles()) {
+      if (await hyde.parseFile(fullpath, this.inputPaths[0]) === undefined) {
+        let content = await readFile(fullpath, 'utf8')
+        let pathComponents = fullpath.split(sep);
+        let outputPath = join(...[
+          this.outputPath,
+          ...pathComponents.slice(pathComponents.indexOf(hyde.name))
+        ]);
+
+        await outputFile(outputPath, content);
+      }
+    }
+
+    for (let item of hyde.items) {
+      let filepath = join(this.outputPath, item.id);
+
+      await outputFile(`${filepath}.md`, item.markdown);
+      await outputJSON(`${filepath}.json`, hyde.serialize(item));
+    }
+
+    for (let collection of hyde.collections) {
+      let filepath = join(this.outputPath, collection.id);
+
+      await outputJSON(`${filepath}.json`, hyde.serialize(collection));
+    }
+  }
+}
